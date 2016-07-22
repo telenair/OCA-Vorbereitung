6e977286bf402b1e70c5c8e056304854df0d57a3
hema <hema@hema-laptop.fritz.box>
2016-07-22 19:18:31 +0200

    test

---
+++ OCA-Vorbereitung/src/oca_vorbereitung/DiesIstEineKlasse.java
@@ -0,0 +1,12 @@
+package oca_vorbereitung;
+
+public class DiesIstEineKlasse {
+
+	public static void main(String[] args) {
+		System.out.println("blubb");
+		System.out.println("ölkjfnw");
+		System.out.println("add to index -> commit -> push");
+		System.out.println("ÖCÖKc ");
+	}
+
+}
